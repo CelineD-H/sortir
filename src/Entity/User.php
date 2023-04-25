be62b0b86d0a851d09ab23c50e62a9fd0279a68e
Celine D-H <cel.dubreuil@gmail.com>
2023-04-25 13:31:35 +0200

    ajout contrainte unicité pseudo + message

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -9,7 +9,8 @@ use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
+#[UniqueEntity(fields: ['email'], message: 'Adresse mail déjà utilisée')]
+#[UniqueEntity(fields: ['pseudo'], message: 'Pseudo déjà utilisé')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
@@ -38,7 +39,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column]
     private ?bool $actif = null;
 
-    #[ORM\Column(length: 30)]
+    #[ORM\Column(length: 30, unique: true)]
     private ?string $pseudo = null;
 
     #[ORM\Column(length: 255)]
