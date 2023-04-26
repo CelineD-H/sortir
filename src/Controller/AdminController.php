ab353596950b8fe35da3f0eecd90e66e44bbc8e9
Mina ZEKRI <mina.zekri2022@campus-eni.fr>
2023-04-26 21:18:42 +0200

    modif dans AdminController

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,9 +13,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class AdminController extends AbstractController
 {
     #[Route('/', name: 'dashboard')]
-    public function index(): Response
+    public function list(Request $request, EntityManagerInterface $entityManager, UserRepository $userRepository): Response
     {
+        $users = $userRepository->findAll();
+
         return $this->render('admin/index.html.twig', [
+            "users" => $users,
             'controller_name' => 'AdminController',
         ]);
     }
