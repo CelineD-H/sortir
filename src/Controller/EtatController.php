9184447c4abac8294c07732b205d60688068f7ff
Mina ZEKRI <mina.zekri2022@campus-eni.fr>
2023-04-26 11:17:24 +0200

    creation entity, controleur, formulaire Etat + ajout navbar

--- src/Controller/EtatController.php
+++ src/Controller/EtatController.php
@@ -2,17 +2,32 @@
 
 namespace App\Controller;
 
+use App\Entity\Etat;
+use App\Form\EtatFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/etat', name: 'etat_')]
+
 class EtatController extends AbstractController
 {
-    #[Route('/etat', name: 'app_etat')]
-    public function index(): Response
+    #[Route('/create', name: 'create')]
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
-        return $this->render('etat/index.html.twig', [
-            'controller_name' => 'EtatController',
+        $etat = new Etat();
+
+        $form = $this->createForm(EtatFormType::class, $etat);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($etat);
+            $entityManager->flush();
+        }
+        return $this->render('etat/create.html.twig', [
+            'etatForm' => $form->createView(),
         ]);
     }
 }
