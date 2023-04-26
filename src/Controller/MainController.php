64c936043f8173adf55d6aa95e780e2d97f76197
Celine D-H <cel.dubreuil@gmail.com>
2023-04-26 14:18:02 +0200

    debut formulaire filtre accueil

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,23 +2,30 @@
 
 namespace App\Controller;
 
+use App\Entity\Sortie;
+use App\Form\SortieFiltreType;
 use App\Repository\SortieRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function list(SortieRepository $sortieRepository): Response
+    public function list(Request $request, EntityManagerInterface $entityManager, SortieRepository $sortieRepository): Response
     {
         if (!$this->getUser()) {
             return $this->redirectToRoute('app_login');
         }
         $sorties = $sortieRepository->findAll();
+        $sortie = new Sortie();
+        $form = $this->createForm(SortieFiltreType::class, $sortie);
 
         return $this->render('main/index.html.twig', [
-            "sorties" => $sorties
+            "sorties" => $sorties,
+            "sortiesForm" => $form->createView()
         ]);
     }
 }
