b041cfa5f3bdd52c571d3800d3b0dae2ebf76458
Mina ZEKRI <mina.zekri2022@campus-eni.fr>
2023-04-26 22:02:29 +0200

    creation des tableaux liste utilisateurs et listes campus

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\CampusRepository;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -13,12 +14,14 @@ use Symfony\Component\Routing\Annotation\Route;
 class AdminController extends AbstractController
 {
     #[Route('/', name: 'dashboard')]
-    public function list(Request $request, EntityManagerInterface $entityManager, UserRepository $userRepository): Response
+    public function list(Request $request, EntityManagerInterface $entityManager, UserRepository $userRepository, CampusRepository $campusRepository): Response
     {
         $users = $userRepository->findAll();
+        $campus = $campusRepository->findAll();
 
         return $this->render('admin/index.html.twig', [
             "users" => $users,
+            "campus" => $campus,
             'controller_name' => 'AdminController',
         ]);
     }
