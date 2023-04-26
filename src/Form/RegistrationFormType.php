3e87758c9dd9afa2bd066e9eed9e63cf2ec1bf49
Sébastien <srouillon@icloud.com>
2023-04-26 13:26:46 +0200

    fix

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -3,26 +3,29 @@
 namespace App\Form;
 
 use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
-use function Sodium\add;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        $campus =
         $builder
             ->add('pseudo')
             ->add('email')
             ->add('firstName')
             ->add('lastName')
             ->add('telephone')
+            ->add('campus', EntityType::class, [
+                'class' => 'App\Entity\Campus',
+                'choice_label' => 'nom'
+            ])
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
