aac03cc3e07da5a3dc156d831799f06e36186149
Celine D-H <cel.dubreuil@gmail.com>
2023-05-02 11:44:53 +0200

    modification mot de passe

--- src/Form/ModificationProfilType.php
+++ src/Form/ModificationProfilType.php
@@ -6,9 +6,11 @@ use App\Entity\User;
 use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\Validator\Constraints\Length;
 use Vich\UploaderBundle\Form\Type\VichImageType;
 use function Sodium\add;
 
@@ -22,6 +24,31 @@ class ModificationProfilType extends AbstractType
             ->add('lastName')
             ->add('email')
             ->add('telephone')
+            ->add('password', PasswordType::class, [
+                'label' => 'Nouveau Mot de passe',
+                'required' => false,
+                'constraints' => [
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                    ])
+                ]
+            ])
+            ->add('confirmationPassword', PasswordType::class, [
+                'label' => 'Confirmation du nouveau Mot de passe',
+                'required' => false,
+                'mapped' => false,
+                'constraints' => [
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                    ])
+                ]
+            ])
             ->add('file', FileType::class,[
                 'label' => 'Avatar',
                 'mapped' => false,
